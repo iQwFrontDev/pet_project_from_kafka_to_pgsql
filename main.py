d443e310f5c83bf37e87d2dca5354d51f0e6b34c
denis <sdamashstroy@mail.ru>
2026-03-24 11:11:22 +0300

    Редактирование main.py

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
 import json
 import time
-import threading
-import psycopg2
-from kafka import KafkaProducer, KafkaConsumer
-from config import Config
+from connction import PostgresConnection, KafkaEventProducer, KafkaEventConsumer
 from generators import EventGenerator
 
-def get_db_connection():
-    return psycopg2.connect(
-        host=Config.DB_HOST.value, port=Config.DB_PORT.value,
-        database=Config.DB_NAME.value, user=Config.DB_USER.value, password=Config.DB_PASSWORD.value
-    )
-
 def run_producer():
-    producer = KafkaProducer(
-        bootstrap_servers=[Config.KAFKA_BOOTSTRAP_SERVERS.value],
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
+ 
+    producer = KafkaEventProducer()
     generator = EventGenerator()
-    print("[Producer] Старт генерации событий...")
+
+    print("Старт генерации событий")
     while True:
         event = generator.get_random_event()
-        producer.send(Config.KAFKA_TOPIC.value, event)
-        print(f"[Producer] Отправлено: {event['event_type']}")
+        producer.send(event) 
+        print(f"Отправлено: {event['event_type']}")
         time.sleep(2)
 
 def run_consumer():
-    conn = get_db_connection()
-    conn.autocommit = True
-    cursor = conn.cursor()
+    db = PostgresConnection()
+    cursor = db.get_cursor()
 
-    consumer = KafkaConsumer(
-        Config.KAFKA_TOPIC.value,
-        bootstrap_servers=[Config.KAFKA_BOOTSTRAP_SERVERS.value],
-        value_deserializer=lambda x: json.loads(x.decode('utf-8')),
-        group_id='stage_loader_group',
-        enable_auto_commit=False,
-        max_poll_records=100
-    )
 
-    print("[Consumer] Старт загрузки в STAGE слой...")
+    consumer = KafkaEventConsumer(group_id='stage_loader_group')
+
+    print("Старт загрузки в STAGE слой")
     while True:
         msg_batches = consumer.poll(timeout_ms=1000)
         if not msg_batches:
@@ -54,7 +37,7 @@ def run_consumer():
                 """, (event['event_type'], json.dumps(event), event['timestamp']))
 
             consumer.commit()
-            print(f"[Consumer] Записано {len(messages)} событий в STAGE.")
+            print(f"Записано {len(messages)} событий в STAGE.")
 
 if __name__ == "__main__":
     time.sleep(10)
